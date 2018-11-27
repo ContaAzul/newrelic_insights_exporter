990ae9ab5f4f90c00a1a1bb0e07af9de1f48b514
guilhermecarneiroo <guilhermecarneiroo@gmail.com>
2018-11-27 13:55:00 -0200

    change to yaml

--- src/helpers/extract-file-data.js
+++ src/helpers/extract-file-data.js
@@ -5,10 +5,13 @@ const getFromFile = function getFromFile() {
   if (fs.existsSync('src/config.yml')) {
     return yaml.safeLoad(fs.readFileSync('src/config.yml', 'utf8'));
   }
+  if (fs.existsSync('src/config.yaml')) {
+    return yaml.safeLoad(fs.readFileSync('src/config.yaml', 'utf8'));
+  }
   if (fs.existsSync('src/config.json')) {
     return JSON.parse(fs.readFileSync('src/config.json', 'utf8'));
   }
-  throw new Error('File not found');
+  throw new Error('Config file not found');
 };
 
 module.exports = {
